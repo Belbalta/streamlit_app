6359e3b3256bd289ca5effc3fc6efda2822ca73d
Belbalta <101603122+Belbalta@users.noreply.github.com>
2022-06-19 12:30:04 -0500

    Update FINAL.py

--- FINAL.py
+++ FINAL.py
@@ -213,7 +213,7 @@ if st.sidebar.button("¿Quiénes somos?"):
 	grayscale = image4.convert('LA')
 	col8.image(grayscale, use_column_width=True)
 	
-	col9 = st.columns(1)
+	col9, col10 = st.columns(2)
 	image5 = Image.open('f8788697-6bbf-4282-b5a8-edb50b7b9cb3.JPG')
 	col9.header("Josmer Cajahuaringa")
 	col9.image(image5, use_column_width=True)
